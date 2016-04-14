a8bb13e0840504652a2c7dc0758be68e8eab5a74
jangrott <tomek.jangrot@gmail.com>
2016-04-14 21:24:50 +0200

    part2

--- reactjs-tutorial/public/app.js
+++ reactjs-tutorial/public/app.js
@@ -1,7 +1,16 @@
 var CommentList = React.createClass({
   render: function() {
+    var comments = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    })
     return (
-      <div className="commentList">I am CommentList</div>
+      <div className="commentList">
+        {comments}
+      </div>
     );
   }
 });
@@ -9,24 +18,54 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
   render: function() {
     return (
-      <div className="commentForm">I am CommentForm</div>
+      <form className="commentForm">
+        <input type="text" placeholder="Your name" />
+        <input type="text" placeholder="Say something" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
 
 var CommentBox = React.createClass({
+  getInitialState() {
+    return {data: []}
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
-        I am CommentBox
-        <CommentList />
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     );
   }
 });
 
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h3 className="commentAuthor">{this.props.author}</h3>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  React.createElement(CommentBox, null),
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 )
